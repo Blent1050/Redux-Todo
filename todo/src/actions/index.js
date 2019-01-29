9a42a9379529a2b3337aec2d4e8ab9fd580673c4
Brandon <Blent1050@yahoo.com>
2019-01-29 15:31:32 -0500

    added todo toggle

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -1,12 +1,30 @@
 export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_FRIEND = 'TOGGLE_FRIEND';
+export const DELETE_FRIEND = 'DELETE_FRIEND';
 
 export const addTodo = value => {
     return{
         type: ADD_TODO,
-        payload: {
+        payload: 
+        {
             value,
+            id: Date.now(),
             completed: false,
-            id: Date.now()
+            
         }
+    }
+}
+
+export const toggleFriend = id => {
+    return{
+        type: TOGGLE_FRIEND,
+        payload: id
+    }
+}
+
+export const deleteFriend = id => {
+    return{
+        type: DELETE_FRIEND,
+        payload: id
     }
 }
--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,5 +1,6 @@
 import {
-    ADD_TODO
+    ADD_TODO,
+    TOGGLE_FRIEND
 } from '../actions';
 
 const initialState = {
@@ -10,9 +11,16 @@ const reducer = (state = initialState, action) => {
     console.log(action.payload, state)
     switch (action.type) {
         case ADD_TODO:
-            return{
+            return {
                 ...state,
                 todos: [...state.todos, action.payload]
+            }
+        case TOGGLE_FRIEND:
+            return {
+                ...state,
+                todos: state.todos.map(todo => todo.id === action.payload ? { ...todo,
+                    completed: !todo.completed
+                } : todo)
             }
         default:
             return state
